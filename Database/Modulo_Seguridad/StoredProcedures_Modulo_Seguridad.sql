779263b50d1ac59285d6637cc6ffd95e3ba502ed
Franco Ibarra <francoibarra_123@live.com.ar>
2014-10-18 03:14:48 -0300

    añadidos scripts y modelo de base

---
+++ Database/Modulo_Seguridad/StoredProcedures_Modulo_Seguridad.sql
@@ -0,0 +1,41 @@
+
+delimiter $$
+drop procedure if exists NuevoUsuario$$
+create procedure NuevoUsuario(P_user varchar(60),
+							  P_pass varchar(60),
+                              P_nombre varchar(80),
+                              P_apellido varchar(80),
+                              P_email varchar(80)
+                              )
+begin
+
+insert into Usuarios(usuario,pass,nombre,apellido,email,estado)
+values(P_user,P_pass,P_nombre,P_apellido,P_email,true);
+
+end$$
+delimiter ;
+
+
+delimiter $$
+drop procedure if exists ModificarUsuario$$
+create procedure ModificarUsuario(P_user varchar(60),
+                                  P_nombre varchar(80),
+                                  P_apellido varchar(80),
+                                  P_email varchar(80),
+                                  P_estado bit
+                                 )
+begin
+
+update Usuarios
+set nombre= P_nombre,
+	apellido = P_apellido,
+    email = P_email,
+    estado = P_estado
+where usuario = P_user;   
+end$$
+delimiter ;
+
+
+
+
+
---
+++ Database/Modulo_Seguridad/tablas_Modulo_seguridad.sql
@@ -0,0 +1,61 @@
+CREATE TABLE `Grupos` (
+  `cod_grupo` varchar(10) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `estado` bit(1) NOT NULL,
+  PRIMARY KEY (`cod_grupo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+create table Usuarios(
+  usuario varchar(16) not null primary key,
+  pass varchar(60) not null,
+  nombre varchar(80) not null,
+  apellido varchar(80) not null,
+  email varchar(120) not null, 
+  estado bit not null			
+ );
+
+create table Usuarios_grupos(
+usuario varchar(16) not null,
+cod_grupo varchar(10) not null, 
+foreign key(usuario) references Usuarios(usuario),
+foreign key(cod_grupo) references Grupos(cod_grupo),
+primary key(usuario,cod_grupo)
+);
+
+
+create table Permisos(
+cod_permiso varchar(10) primary key,
+descripcion varchar(100) not null
+);
+
+create table Modulos(
+cod_modulo varchar(10) primary key,
+descripcion varchar(100) not null
+
+);
+
+
+create table Formularios(
+cod_formulario varchar(10) primary key,
+descripcion varchar(100) not null,
+cod_modulo varchar(10) not null,
+foreign key(cod_modulo) references Modulos(cod_modulo)
+);
+
+create table Perfiles(
+cod_grupo varchar(10),
+cod_formulario varchar(10),	
+cod_permiso varchar(10),
+primary key(cod_grupo,cod_formulario,cod_permiso),
+foreign key(cod_grupo) references Grupos(cod_grupo),
+foreign key(cod_formulario) references Formularios(cod_formulario),
+foreign key(cod_permiso) references Permisos(cod_permiso)
+);
+
+
+
+
+
+
+
