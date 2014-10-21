32714e4b045188662c24cbfe8e5192b1cee605ba
4riel <ariel.marti@hotmail.com>
2014-10-21 15:43:34 -0300

    vista

---
+++ App/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Modulo de Seguridad</title>
+</head>
+<body>
+	<?php include('vista/includes/header.php'); ?>
+</body>
+</html>
---
+++ App/vista/includes/header.php
@@ -0,0 +1,9 @@
+<header id="" class="">
+	<h1>Modulo de Seguridad</h1>
+	<ul>
+		<li>
+			<a href="" title="">Gestion de Usuarios</a>
+			<a href="" title="">Gestion de Grupos</a>
+		</li>
+	</ul>
+</header>
